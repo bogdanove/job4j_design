4c40e63f296889b28d8d0b3e98b697f22467ee17
bogdanove <anoth3rx@gmail.com>
2022-06-15 18:48:37 +0300

    5.2. Архивировать проект [#861 #312457]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -3,17 +3,16 @@ package ru.job4j.io;
 import java.io.*;
 import java.nio.file.Path;
 import java.util.List;
-import java.util.stream.Collectors;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-    public void packFiles(List<File> sources, File target) {
+    public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            for (File file : sources) {
-                if (file.exists()) {
-                    zip.putNextEntry(new ZipEntry(file.getPath()));
-                    try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(file))) {
+            for (Path path : sources) {
+                if (path.toFile().exists()) {
+                    zip.putNextEntry(new ZipEntry(path.toString()));
+                    try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(path.toString()))) {
                         zip.write(out.readAllBytes());
                     }
                 }
@@ -23,28 +22,22 @@ public class Zip {
         }
     }
 
-    public void packSingleFile(File source, File target) {
-        try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(new FileOutputStream(target)))) {
-            zip.putNextEntry(new ZipEntry(source.getPath()));
-            try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(source))) {
-                zip.write(out.readAllBytes());
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+        ArgsName name = validateArgs(args);
+        File root = new File(name.get("d"));
+        List<Path> search = Search.search(root.toPath(), c -> !c.toFile().getName().endsWith(name.get("e")));
+        Zip zip = new Zip();
+        zip.packFiles(
+                search,
+                new File(name.get("o"))
+        );
     }
 
-    public static void main(String[] args) throws IOException {
+    private static ArgsName validateArgs(String[] args) {
         ArgsName name = ArgsName.of(args);
-        if (!name.get("d").isEmpty() && !name.get("e").isEmpty() && !name.get("o").isEmpty()) {
-            File root = new File(name.get("d"));
-            List<Path> search = Search.search(root.toPath(), c -> !c.toFile().getName().endsWith(name.get("e")));
-            List<File> files = search.stream().map(Path::toFile).collect(Collectors.toList());
-            Zip zip = new Zip();
-            zip.packFiles(
-                    files,
-                    new File(name.get("o"))
-            );
+        if (args.length == 0 || name.get("d").isEmpty() || name.get("e").isEmpty() || name.get("o").isEmpty()) {
+            throw new IllegalArgumentException();
         }
+        return name;
     }
 }
