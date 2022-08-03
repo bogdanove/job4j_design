8bbd39d1559410ce204da7bc0fbb082e76828ac9
bogdanove <anoth3rx@gmail.com>
2022-08-03 20:21:20 +0300

    0.2. PreparedStatement [#379307 #327782]

--- src/main/java/ru/job4j/jdbc/ImportDB.java
+++ src/main/java/ru/job4j/jdbc/ImportDB.java
@@ -23,13 +23,14 @@ public class ImportDB {
         List<User> users = new ArrayList<>();
         try (BufferedReader rd = new BufferedReader(new FileReader(dump))) {
             rd.lines().forEach(line -> {
-                        if (line.split(";").length != 2 || line.split(";")[0].isEmpty() || line.split(";")[1].isEmpty()) {
+                String[] lines = line.split(";");
+                        if (lines.length != 2 || lines[0].isEmpty() || lines[1].isEmpty()) {
                             throw new IllegalArgumentException("Wrong data!");
                         }
                         users.add(
                                 new User(
-                                        line.split(";")[0],
-                                        line.split(";")[1]
+                                        lines[0],
+                                        lines[1]
                                 )
                         );
                     }
