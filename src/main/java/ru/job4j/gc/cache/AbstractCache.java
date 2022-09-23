199c152c661e203739c4d157d5ef5a3e6493b595
bogdanove <anoth3rx@gmail.com>
2022-09-23 20:24:28 +0300

    1. Реализация кеша на SoftReference [#1592 #345368]

--- src/main/java/ru/job4j/gc/cache/AbstractCache.java
+++ src/main/java/ru/job4j/gc/cache/AbstractCache.java
@@ -16,8 +16,8 @@ public abstract class AbstractCache<K, V> {
     public V get(K key) {
         var result = cache.getOrDefault(key, new SoftReference<>(null)).get();
         if (result == null) {
-            put(key, load(key));
-            result = get(key);
+            result = load(key);
+            put(key, result);
         }
         return result;
     }
