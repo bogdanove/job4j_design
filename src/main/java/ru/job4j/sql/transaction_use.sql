7d57f1f12ec7a276d22c8c04ab97994d309d3b14
bogdanove <anoth3rx@gmail.com>
2022-09-23 20:31:48 +0300

    4.4. Транзакции. Работа с транзакциями в PostgreSQL [#504813]

---
+++ src/main/java/ru/job4j/sql/transaction_use.sql
@@ -0,0 +1,52 @@
+create table products
+(
+    id       serial primary key,
+    name     varchar(50),
+    producer varchar(50),
+    count    integer default 0,
+    price    integer
+);
+
+insert into products (name, producer, count, price)
+VALUES ('product_1', 'producer_1', 3, 50);
+insert into products (name, producer, count, price)
+VALUES ('product_2', 'producer_2', 15, 32);
+insert into products (name, producer, count, price)
+VALUES ('product_3', 'producer_3', 8, 115);
+
+begin
+transaction;
+
+insert into products (name, producer, count, price)
+VALUES ('product_4', 'producer_4', 11, 64);
+
+savepoint first_savepoint;
+
+delete
+from products
+where name = 'product_1';
+
+savepoint second_savepoint;
+
+select *
+from products;
+
+drop table products;
+
+rollback to second_savepoint;
+
+insert into products (name, producer, count, price)
+VALUES ('product_1', 'producer_1', 3, 50);
+
+select *
+from products;
+
+rollback to first_savepoint;
+
+insert into products (name, producer, count, price)
+VALUES ('product_1', 'producer_1', 3, 50);
+
+select *
+from products;
+
+commit;
