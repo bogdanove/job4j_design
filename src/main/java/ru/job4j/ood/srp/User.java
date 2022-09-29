aaa6db20c18fdeb23961a7c8ec4a5c3eb5dc48f9
bogdanove <anoth3rx@gmail.com>
2022-09-29 21:21:40 +0300

    0. Принцип единственной ответственности [#4913 #346658]

--- src/main/java/ru/job4j/ood/srp/User.java
+++ src/main/java/ru/job4j/ood/srp/User.java
@@ -1,16 +1,43 @@
 package ru.job4j.ood.srp;
 
-public class User {
+import java.util.Objects;
 
+public class User {
     /*
      *
      * Этот класс нарушает принцип SRP потому что в нем реализуется дополнительная логика.
      *
      */
-
     private String name;
     private String address;
     public boolean isHumanioid() {
         return name.isEmpty() && address.isEmpty();
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return Objects.equals(name, user.name) && Objects.equals(address, user.address);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, address);
     }
 }
