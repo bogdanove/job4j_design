775cfac15462103e2e7a2a5d8bda27e766352469
bogdanove <anoth3rx@gmail.com>
2022-10-19 19:33:23 +0300

    1. Хранилище продуктов [#852 #349424]

--- src/main/java/ru/job4j/ood/lsp/store/Shop.java
+++ src/main/java/ru/job4j/ood/lsp/store/Shop.java
@@ -19,7 +19,7 @@ public class Shop extends AbstractStore {
     public boolean add(Food food) {
         boolean result = false;
         var check = checker.getExpirationPercentage(food.getCreateDate(), food.getExpiryDate());
-        if (check >= FreshnessQuality.GOOD && check <= FreshnessQuality.BAD) {
+        if (check >= FreshnessQuality.GOOD && check < FreshnessQuality.BAD) {
             if (check > FreshnessQuality.MIDDLE) {
                 food.setDiscount(FreshnessQuality.DISCOUNT);
                 food.setPrice(food.getPrice() * food.getDiscount());
