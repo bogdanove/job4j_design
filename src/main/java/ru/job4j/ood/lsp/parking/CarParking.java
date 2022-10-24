4f619944ffbdd2daf5bcd590b8f7e1b20ea2f3cc
bogdanove <anoth3rx@gmail.com>
2022-10-24 19:18:19 +0300

    2. Парковка машин [#853 #352394]

--- src/main/java/ru/job4j/ood/lsp/parking/CarParking.java
+++ src/main/java/ru/job4j/ood/lsp/parking/CarParking.java
@@ -1,24 +1,35 @@
 package ru.job4j.ood.lsp.parking;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class CarParking implements Parking {
-
-    protected final Car[] passengerParking;
-
-    protected final Car[] truckParking;
-
+    protected final List<Car> passengerParking;
+    protected final List<Car> truckParking;
     private final int passengerPlace;
-
     private final int truckPlace;
 
     public CarParking(int passengerPlace, int truckPlace) {
         this.passengerPlace = passengerPlace;
         this.truckPlace = truckPlace;
-        this.passengerParking = new Car[passengerPlace];
-        this.truckParking = new Car[truckPlace];
+        this.passengerParking = new ArrayList<>(passengerPlace);
+        this.truckParking = new ArrayList<>(truckPlace);
     }
 
     @Override
     public boolean add(Car car) {
-        return false;
+        var result = false;
+        if (car.getSize() == PassengerCar.PASSENGER_SIZE && passengerParking.size() < passengerPlace) {
+            result = passengerParking.add(car);
+        }
+        if (car.getSize() > PassengerCar.PASSENGER_SIZE) {
+            if (truckParking.size() < truckPlace) {
+                result = truckParking.add(car);
+            } else if (passengerParking.size() < passengerPlace
+                    && car.getSize() <= (passengerPlace - passengerParking.size())) {
+                result = passengerParking.add(car);
+            }
+        }
+        return result;
     }
 }
--- src/test/java/ru/job4j/ood/lsp/parking/CarParkingTest.java
+++ src/test/java/ru/job4j/ood/lsp/parking/CarParkingTest.java
@@ -1,11 +1,9 @@
 package ru.job4j.ood.lsp.parking;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Disabled
 class CarParkingTest {
 
     @Test
